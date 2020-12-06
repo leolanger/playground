2944c22cfee946091a25571629e4df3041ea288b
leolanger <leolanger2001@outlook.com>
2020-12-07 00:12:48 +0800

    do

--- haskell/What_I_whish/Monad/src/IO.hs
+++ haskell/What_I_whish/Monad/src/IO.hs
@@ -1,12 +1,21 @@
 module IO where
 
-import Prelude hiding (Monad, return, (>>=))
+import GHC.IO.Handle
+import GHC.IO.Handle.FD (stdout)
+import GHC.IO.Handle.Text (hPutStrLn)
+import Prelude (Bool, IO, String)
 
 class Monad m where
   return :: a -> m a
   (>>=) :: m a -> (a -> m b) -> m b
-
-(>>) :: Monad m => m a -> m b -> m b
-m >> k = m >>= \_ -> k
+  (>>) :: m a -> m b -> m bk
 
 {- IO Monad-}
+
+putStrLn :: String -> IO ()
+putStrLn s = hPutStrLn stdout s
+
+main :: IO ()
+main = do putStrLn "Vesihiisi sihisi hississäään."
+
+getLine :: IO String
