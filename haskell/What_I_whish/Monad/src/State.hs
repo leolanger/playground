607d6ed5ce0e5360c6fb33e714775ccb8c7c8f3c
leolanger <leolanger2001@outlook.com>
2020-12-09 23:57:53 +0800

    modify

--- haskell/What_I_whish/Monad/src/State.hs
+++ haskell/What_I_whish/Monad/src/State.hs
@@ -5,12 +5,20 @@ module State where
 newtype State s a = State {runState :: s -> (a, s)}
 
 instance Functor (State s) where
-    fmap = 
+  fmap f (State x) = State $ \s ->
+    let (a, s1) = x s
+     in (f a, s1)
 
 instance Applicative (State s) where
+  pure x = State $ \s -> (x, s)
+  (State f) <*> (State g) =
+    State $ \s ->
+      let (func, s1) = f s
+          (val, s2) = g s1
+       in (func val, s2)
 
 instance Monad (State s) where
   return a = State $ \s -> (a, s)
   State act >>= k = State $ \s ->
-    let (a, s') = act s
-     in runState (k a) s'
+    let (a, s1) = act s
+     in runState (k a) s1
