8946c60901d13ca5879c9c0c94b562e69cf34f70
leolanger <leolanger2001@outlook.com>
2020-12-14 23:47:19 +0800

    do experiment

--- haskell/What_I_whish/LanguageExtensions/src/Extension4.hs
+++ haskell/What_I_whish/LanguageExtensions/src/Extension4.hs
@@ -1,1 +1,36 @@
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE RecursiveDo #-}
+
 module Extension4 where
+
+{- PartialTypeSignatures -}
+
+-- Normally a function is either given a full explicit type signature or none at all.
+-- The partial type signature extension allows something in between.
+
+-- triple :: Int -> ((Int, Int, Int))
+triple :: Int -> _
+triple i = (i, i, i)
+
+{- RecursiveDo -}
+
+-- Recursive do notation allows for the use of self­reference expressions on both sides of a monadic bind.
+
+justOnes :: Maybe [Int]
+justOnes = do
+  rec xs <- Just (1 : xs)
+  return (map negate xs)
+
+{- ApplicativeDo -}
+
+test :: Applicative m => m (a, b, c)
+test = do
+  a <- f
+  b <- g
+  c <- h
+  return (a, b, c)
+
+-- equivalent to the traditional notation
+
+test :: Applicative m => m (a, b, c)
+test = (,,) <$> f <*> g <*> h
