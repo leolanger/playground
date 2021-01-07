a6a2bca1f74c78056f8adce615d7249800139e7a
leolanger <leolanger2001@outlook.com>
2021-01-07 22:46:37 +0800

    do experiment

--- rust/test0/src/main.rs
+++
@@ -1,2 +0,0 @@
-use heapless::consts::*;
-use heapless::spsc::Queue;
--- rust/test0/target/debug/deps/test0-c6da4a3a4ada2ec7.d
+++
@@ -1,5 +0,0 @@
-/home/leolanger/projects/playground/rust/test0/target/debug/deps/test0-c6da4a3a4ada2ec7.rmeta: src/main.rs
-
-/home/leolanger/projects/playground/rust/test0/target/debug/deps/test0-c6da4a3a4ada2ec7.d: src/main.rs
-
-src/main.rs:
