18788d98b63ba2f3fa56b21c98ba3046e5e7efaf
leolanger <leolanger2001@outlook.com>
2021-01-09 22:12:13 +0800

    do exercise

--- rust/rust-crash-course/bo/target/debug/bo.d
+++
@@ -1,1 +0,0 @@
-/home/leolanger/projects/playground/rust/rust-crash-course/bo/target/debug/bo: /home/leolanger/projects/playground/rust/rust-crash-course/bo/src/main.rs
--- rust/rust-crash-course/bo/target/debug/deps/bo-10f77cff67f95c91.d
+++
@@ -1,5 +0,0 @@
-/home/leolanger/projects/playground/rust/rust-crash-course/bo/target/debug/deps/bo-10f77cff67f95c91.rmeta: src/main.rs
-
-/home/leolanger/projects/playground/rust/rust-crash-course/bo/target/debug/deps/bo-10f77cff67f95c91.d: src/main.rs
-
-src/main.rs:
